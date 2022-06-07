7592ff62d5496336e511b133ff0ba7d6cad58ad1
Relyg15 <98603697+Relyg15@users.noreply.github.com>
2022-06-07 18:15:41 -0400

    Update IXLUserDataCollector.java

--- src/IXLUserDataCollector.java
+++ src/IXLUserDataCollector.java
@@ -11,38 +11,38 @@ public class IXLUserDataCollector {
     public IXLUserDataCollector(String userName) {
         this.userName = userName;
         userNames = new ArrayList<String>();
-        filePath = "C://IntelliJ Folder//IXLProject_Redo//src//userdata.txt";
-        //filePath = "C://Users//BT_1E10_30//relyganesh//src//userdata.data";
+        filePath = "src/userdata.txt";
+        
     }
 
 
     public void createNewUserData() {
-if (isNewUserData()) {
-    try {
+    if (isNewUserData()) {
+        try {
         File f = new File(filePath);
         f.createNewFile();
         FileWriter fw = new FileWriter(f, true);
         fw.write(userName + "\n");
         fw.close();
-    } catch (IOException e) {
+                 }           
+        catch (IOException e) {
         System.out.println("Unable to create file");
         e.printStackTrace();
-    }
-}
+                 }
+            }
         }
 
     public boolean isNewUserData(){
         if (userNames.contains(userName)){
             System.out.println("Welcome back " + userName);
             return false;
-        }
+             }
         else {
             System.out.println("Welcome to IXL " + userName);
             return true;
-        }
-
-    }
-
+             }
+      }
+   
     public void getUserInfo() throws IOException {
         userNames = Files.readAllLines(Paths.get(filePath));
     }
